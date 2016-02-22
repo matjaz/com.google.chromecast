3d5f66ee000a5001a08e2f9a74a0be71c183b2a1
Emile Nijssen <emilenijssen@gmail.com>
2016-02-22 23:02:17 +0100

    youtube search working

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
 'use strict'
 
-exports.init = function () {
-  Homey.manager('flow').on('action.castVideo', onFlowActionCastVideo)
+var YouTube				= require('youtube-node');
+var youTube;
+
+exports.init = function() {
+	
+	youTube = new YouTube();
+	youTube.setKey( Homey.env.YOUTUBE_KEY );
+		
+	Homey.manager('flow').on('action.castYouTube', onFlowActionCastYouTube)
+	Homey.manager('flow').on('action.castYouTube.youtube_id.autocomplete', onFlowActionCastYouTubeAutocomplete);
+	Homey.manager('flow').on('action.castVideo', onFlowActionCastVideo)
 }
 
-function onFlowActionCastVideo (callback, args) {
-  Homey
-  	.manager('drivers')
-  	.getDriver('chromecast')
-  	.playVideo(args.chromecast.id, args.url, callback)
+function onFlowActionCastYouTube(callback, args) {
+	console.log(args)
+	Homey.manager('drivers')
+		.getDriver('chromecast')
+		.playVideo(args.chromecast.id, 'https://www.youtube.com/watch?v=' + args.youtube_id.id, callback)
+}
+
+function onFlowActionCastYouTubeAutocomplete( callback, args ){
+				
+	youTube.addParam('type', 'video');
+	youTube.search(args.query, 5, function(error, result) {
+		if (error) return;
+		
+		var videos = [];
+		result.items.forEach(function(video){
+			videos.push({
+				id		: video.id.videoId,
+				name	: video.snippet.title,
+				image	: video.snippet.thumbnails.default.url
+			})
+		})
+					
+		callback( null, videos );
+	});
+	
+}
+
+function onFlowActionCastVideo(callback, args) {
+	Homey.manager('drivers')
+		.getDriver('chromecast')
+		.playVideo(args.chromecast.id, args.url, callback)
 }
--- drivers/chromecast/driver.js
+++ drivers/chromecast/driver.js
@@ -1,121 +1,102 @@
 'use strict'
-
-var ytdl     = require('ytdl-core')
+var ytdl = require('ytdl-core')
 var ytdlUtil = require('ytdl-core/lib/util')
 var ChromecastAPI = require('chromecast-api')
-
 var devices = []
-
-exports.init = function (devices, callback) {
-
-  // Homey.log('init', devices)
-
-  discoverChromecasts()
-
-  callback()
+exports.init = function(devices, callback) {
+	// Homey.log('init', devices)
+	discoverChromecasts()
+	callback()
 }
 
-exports.capabilities = {
-
+exports.capabilities = {}
+exports.pair = function(socket) {
+	socket.on('list_devices', function(data, callback) {
+		callback(null, devices.map(function(chromecast) {
+			return {
+				name: chromecast.config.name,
+				data: {
+					id: chromecast.config.name,
+					ip: chromecast.host
+				}
+			}
+		}))
+	})
+}
+exports.playVideo = function(deviceName, videoUrl, callback) {
+		
+	// Homey.log(deviceName, devices)
+	var device = devices.filter(function(device) {
+		return device.config && device.config.name === deviceName
+	})[0]
+	// Homey.log('device', device)
+	if (device) {
+		getVideoInfo(videoUrl, function(err, media) {
+			if (err) {
+				Homey.error(err)
+				callback && callback(err)
+				return
+			}
+			// Homey.log('media', media)
+			device.play(media, 0, function() {
+				Homey.log('Playing on your chromecast')
+				callback && callback(null, true)
+			})
+		})
+	} else if (callback) {
+		callback(new Error('Device ' + deviceName + 'not found'))
+	}
 }
 
-exports.pair = function (socket) {
-  socket.on('list_devices', function (data, callback) {
-    callback(null, devices.map(function (chromecast) {
-        return {
-          name: chromecast.config.name,
-          data: {
-            id: chromecast.config.name,
-            ip: chromecast.host
-          }
-        }
-     }))
-  })
+function discoverChromecasts() {
+	var browser = new ChromecastAPI.Browser()
+	browser.on('deviceOn', function(device) {
+		devices.push(device)
+		// Homey.log('devices', devices)
+	})
+	// Homey.log('devices', devices)
 }
 
-exports.playVideo = function (deviceName, videoUrl, callback) {
-  // Homey.log(deviceName, devices)
-  var device = devices.filter(function (device) {
-    return device.config && device.config.name === deviceName
-  })[0]
-  // Homey.log('device', device)
-  if (device) {
-    getVideoInfo(videoUrl, function (err, media) {
-      if (err) {
-      	Homey.error(err)
-      	callback && callback(err)
-      	return
-      }
-
-      // Homey.log('media', media)
-      device.play(media, 0, function () {
-        // Homey.log('Playing in your chromecast')
-        callback && callback(null, true)
-      })
-    })
-  } else if (callback) {
-  	callback(new Error('Device ' + deviceName + 'not found'))
-  }
+function getVideoInfo(url, callback) {
+	if (isYoutubeVideo(url)) {
+		var options = {
+			filter: function(format) {
+				return format.type.indexOf('video/mp4') === 0
+			}
+		}
+		getYTVideoInfo(url, options, function(err, info) {
+			if (err) return callback(err)
+			// Homey.log('YT info', info)
+			callback(null, {
+				url: info.url,
+				cover: {
+					title: info.title,
+					url: info.iurlmaxres
+				}
+			})
+		})
+	} else {
+		callback(null, {
+			url: url
+		})
+	}
 }
 
-function discoverChromecasts () {
-  var browser = new ChromecastAPI.Browser()
-
-  browser.on('deviceOn', function (device) {
-    devices.push(device)
-    // Homey.log('devices', devices)
-  })
-
-  setTimeout(function () {
-    // Homey.log('stop discover', devices)
-    browser.stop()
-    browser = null
-  }, 60000)
-
-  // Homey.log('devices', devices)
+function isYoutubeVideo(url) {
+	return /(?:youtu\.be)|(?:youtube\.com)/.test(url)
 }
 
-function getVideoInfo (url, callback) {
-  if (isYoutubeVideo(url)) {
-    var options = {
-      filter: function (format) {
-        return format.type.indexOf('video/mp4') === 0
-      }
-    }
-    getYTVideoInfo(url, options, function (err, info) {
-      if (err) return callback(err)
-
-      // Homey.log('YT info', info)
-      callback(null, {
-        url: info.url,
-        cover: {
-          title: info.title,
-          url: info.iurlmaxres
-        }
-      })
-    })
-  } else {
-    callback(null, {
-      url: url
-    })
-  }
-}
-
-function isYoutubeVideo (url) {
-  return /(?:youtu\.be)|(?:youtube\.com)/.test(url)
-}
-
-function getYTVideoInfo (url, options, callback) {
-  ytdl.getInfo(url, function (err, info) {
-    if (err) {
-      callback(err)
-      return
-    }
-    var format = ytdlUtil.chooseFormat(info.formats, options)
-    if (format instanceof Error) {
-      callback(format)
-    } else {
-      callback(null, format)
-    }
-  })
+function getYTVideoInfo(url, options, callback) {
+	ytdl.getInfo(url, function(err, info) {
+		if (err) {
+			callback(err)
+			return
+		}
+		var format = ytdlUtil.chooseFormat(info.formats, options)
+		if (format instanceof Error) {
+			callback(format)
+		} else {
+			callback(null, format)
+		}
+	})
 }
