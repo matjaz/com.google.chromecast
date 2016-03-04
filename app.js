b504edcd087b3c02baffac8b60c11423025c2bd9
Matjaz Lipus <matjazl@gmail.com>
2016-03-04 19:08:40 +0100

    cleanup

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 'use strict'
 
-var YouTube				= require('youtube-node');
+var YouTube = require('youtube-node');
 var youTube;
 
 exports.init = function() {
 	
 	youTube = new YouTube();
 	youTube.setKey( Homey.env.YOUTUBE_KEY );
+	youTube.addParam('type', 'video');
 		
 	Homey.manager('flow').on('action.castYouTube', onFlowActionCastYouTube)
 	Homey.manager('flow').on('action.castYouTube.youtube_id.autocomplete', onFlowActionCastYouTubeAutocomplete);
@@ -14,15 +15,12 @@ exports.init = function() {
 }
 
 function onFlowActionCastYouTube(callback, args) {
-	console.log(args)
 	Homey.manager('drivers')
 		.getDriver('chromecast')
 		.playVideo(args.chromecast.id, 'https://www.youtube.com/watch?v=' + args.youtube_id.id, callback)
 }
 
 function onFlowActionCastYouTubeAutocomplete( callback, args ){
-				
-	youTube.addParam('type', 'video');
 	youTube.search(args.query, 5, function(error, result) {
 		if (error) return;
 		
